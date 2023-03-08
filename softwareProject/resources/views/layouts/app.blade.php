c458a3f94408ae7e471239f15bf1e8e7faf17a4c
N00212147 <N00212147@iadt.ie>
2023-03-08 17:38:37 +0000

    Developed the user profile page visually.

--- softwareProject/resources/views/layouts/app.blade.php
+++ softwareProject/resources/views/layouts/app.blade.php
@@ -72,9 +72,9 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        
             @yield('content')
-        </main>
+        
     </div>
 </body>
 </html>
--- softwareProject/resources/views/user/profile/index.blade.php
+++ softwareProject/resources/views/user/profile/index.blade.php
@@ -1,23 +1,129 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="container-lg">
 
-            {{-- header --}}
-            <div class="row">
-                <div class="col-6">
+<div class="container-fluid ">
+    <div class="container-lg mb-8">
+        <div class="row">
+            <div class="col-3">
+                <p class="fs-2 mt-5 mb-0">
+                    {{-- displaying users name --}}
+                    {{Auth::user()->name}}
+                </p>
 
-                    {{Auth::user()->profile_img}}
+                {{-- displays users profile image --}}
+                <div class="profile-bg-user py-8 fw-bold text-light text-center img-fluid">
+                    USER PROFILE IMAGE HERE
+                </div>
+                <div>
+                    <p class="fw-semibold fs-3">Skillset</p>
 
-                      <p>Skillset</p>
-                      <td>
-                        {{Auth::user()->name}}
-                      </td>
+                        <div class="mb-2">
+                            <p class="mb-0 fw-semibold text-muted fs-5">Endurance</p>
+                            {{-- traffic light system --}}
+                            <button class="bg-success px-4 py-2"></button> 
+                            <button class="bg-success px-4 py-2"></button> 
+                            <button class="bg-success px-4 py-2"></button> 
+                            <button class="bg-success px-4 py-2"></button> 
+                            <button class="bg-success px-4 py-2"></button>
+                        </div>
+
+                        <div class="mb-2">
+                            <p class="mb-0 fw-semibold text-muted fs-5">Slab</p>
+                            {{-- traffic light system --}}
+                            <button class="bg-warning px-4 py-2"></button> 
+                            <button class="bg-warning px-4 py-2"></button> 
+                            <button class="bg-warning px-4 py-2"></button> 
+                            <button class="bg-dark px-4 py-2"></button> 
+                            <button class="bg-dark px-4 py-2"></button>
+                        </div>
+
+                        <div class="mb-2">
+                            <p class="mb-0 fw-semibold text-muted fs-5">Technique</p>
+                            {{-- traffic light system --}}
+                            <button class="bg-danger px-4 py-2"></button> 
+                            <button class="bg-danger px-4 py-2"></button> 
+                            <button class="bg-dark px-4 py-2"></button> 
+                            <button class="bg-dark px-4 py-2"></button> 
+                            <button class="bg-dark px-4 py-2"></button>
+                        </div>
                 </div>
             </div>
+
+        {{-- contact user button/next user --}}
+        <div class="col-9 mt-8">
+            <button type="button" class="btn btn-orange btn-outline-orangeStroke btn-lg col-9 text-light">Contact {{Auth::user()->name}}</button>
+
+                <button type="button" class="btn btn-orange btn-outline-orangeStroke text-light btn-lg col-2">Next User</button>
+
+                <p class="col-9 mt-2 fw-light fs-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt velit, qui maxime doloribus laudantium explicabo! Sit dolorem aut ex natus eveniet, autem vero officiis itaque rem accusantium ducimus quae dolorum voluptatum ratione quaerat corrupti? Rem quo error vitae harum esse molestiae velit. Laudantium accusantium reiciendis hic Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt velit, qui maxime doloribus laudantium explicabo! Sit dolorem aut ex natus eveniet, autem vero officiis itaque rem accusantium ducimus quae dolorum voluptatum ratione quaerat corrupti? Rem quo error vitae harum esse molestiae velit. Laudantium accusantium reiciendis hic </p>
         </div>
     </div>
+</div>
+</div>
+
+{{-- footer --}}
+<div class="container-fluid bg-dark">
+<div class="container-lg">
+    <footer class="py-5">
+      <div class="row">
+        <div class="col-6 col-md-2 mb-3">
+          <h5 class="text-light">Section</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+  
+        <div class="col-6 col-md-2 mb-3">
+          <h5 class="text-light">Section</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+  
+        <div class="col-6 col-md-2 mb-3">
+          <h5 class="text-light">Section</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+  
+        <div class="col-md-5 offset-md-1 mb-3">
+          <form>
+            <h5 class="text-light">Subscribe to our newsletter</h5>
+            <p class="text-muted">Monthly digest of what's new and exciting from us.</p>
+            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+              <label for="newsletter1" class="visually-hidden">Email address</label>
+              <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+              <button class="btn btn-orange text-light" type="button">Subscribe</button>
+            </div>
+          </form>
+        </div>
+      </div>
+  
+      <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+        <p class="text-muted">&copy; 2022 Company, Inc. All rights reserved.</p>
+        <ul class="list-unstyled d-flex">
+          <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+          <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+          <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+</div>
 
 
 
